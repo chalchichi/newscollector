7c0c76810fe7a24ee476bef898b2a25775183b22
ohyunhu <dhdbsgn111@naver.com>
2021-01-18 00:52:00 +0900

    로그인시 Guser 테이블에서 Account에 저장

--- src/main/java/com/example/newscollector/Controller/IndexController.java
+++ src/main/java/com/example/newscollector/Controller/IndexController.java
@@ -1,16 +1,32 @@
 package com.example.newscollector.Controller;
 
+import com.example.newscollector.JPA_entity.Account;
+import com.example.newscollector.JPA_entity.Collection;
+import com.example.newscollector.JPA_repository.AccountRepository;
+import com.example.newscollector.JPA_repository.CollectionRepository;
+import com.example.newscollector.JPA_repository.NewsRepository;
 import com.example.newscollector.Oauth.SessionUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    CollectionRepository collectionRepository;
+
+    @Autowired
+    NewsRepository newsRepository;
 
     private final HttpSession httpSession;
 
@@ -19,7 +35,9 @@ public class IndexController {
 
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
-            System.out.println(user.getName());
+            Account account = new Account();
+            account.setUserid(user.getName());
+            accountRepository.save(account);
             model.addAttribute("username", user.getName());
             model.addAttribute("picture", user.getPicture());
         }
@@ -27,4 +45,11 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/data")
+    public List<Collection> getData()
+    {
+        Account getaccount = accountRepository.findAccountByUserid("ohyunhoo");
+        List<Collection> listbyuser = collectionRepository.findCollectionsByAccount(getaccount);
+        return listbyuser;
+    }
 }
